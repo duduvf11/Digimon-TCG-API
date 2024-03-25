b2d487d2dfbedef1b7c45a19458bde515ccc3171
Eduardo Viegas Francisco <duduviegasfrancisco11@gmail.com>
2024-03-25 16:06:13 -0300

    error verify

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function fetchDigAPI(){
     fetch('https://digimoncard.io/api-public/getAllCards.php?sort=name&series=Digimon Card Game&sortdirection=asc')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         const list = document.querySelector('#fill_list')
         list.innerHTML = ''
         
@@ -21,22 +20,37 @@ function fetchDigAPI(){
 function searchDigAPI(){
 
         const value = document.querySelector('#search_input').value
+        
         fetch('https://digimoncard.io/api-public/search.php?n=' + value)
-            .then((res) => res.json())
-            .then((data) => {
-                const search_list = document.querySelector('#search_list')
-                search_list.innerHTML = ''
-                    
-                data.map((item) => {
-                    const li = document.createElement('li')
+        .then((res) => res.json())
+        .then((data) => {
+            const search_list = document.querySelector('#search_list')
+            search_list.innerHTML = ''
+            
+            if (value === '') {
+                const errorLi = document.createElement('li')
+                errorLi.textContent = 'Por favor, insira um valor no campo de pesquisa.'
+                search_list.appendChild(errorLi)
+                return;
+            }
 
-                    li.setAttribute('id', item.id)
-                    li.innerHTML = `name: ${item.name}<br />card number: ${item.cardnumber} <br /> digi_type: ${item.digi_type} <br /> image: <br><img src="${item.image_url}"></img><br /> <br />`
-                    search_list.appendChild(li)
-                })
-                document.querySelector('input').value = ''
+            if (!Array.isArray(data)) {
+                const errorLi = document.createElement('li')
+                errorLi.textContent = 'Nenhum resultado encontrado.'
+                search_list.appendChild(errorLi)
+                return;
+            }
+
+            data.forEach((item) => {
+                const li = document.createElement('li')
+
+                li.setAttribute('id', item.id)
+                li.innerHTML = `name: ${item.name}<br />card number: ${item.cardnumber} <br /> digi_type: ${item.digi_type} <br /> image: <br><img src="${item.image_url}"></img><br /> <br />`
+                search_list.appendChild(li)
             })
-            .catch((err) => console.log(err));
+            document.querySelector('input').value = ''
+        })
+        .catch((err) => console.error(err))
 }
 
 function limparPesquisa(){
