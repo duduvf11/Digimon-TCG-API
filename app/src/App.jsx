6f459afa07fdd799d0c248ccef68121fae48c7c9
rodrigoaleixoff <rodrigo.aff22@gmail.com>
2024-04-24 18:01:16 -0300

    ready

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,122 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [showDigimonAPI, setShowDigimonAPI] = useState(false)
+    
+    return <div>
+        <h1>Digimon API</h1>
+        <hr />
+        <DigimonSearchAPI/>
+        <button onClick={() => setShowDigimonAPI(true)}>Preencher</button>
+        {showDigimonAPI && <DigimonAPI/>}
+    </div>
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function DigimonSearchAPI(){
+    const [input, setInput] = useState('')
+    const [send, setSend] = useState('')
+
+    return (
+        <div>
+            <div>
+               <input type="text" value={input} onChange={(ev) => setInput(ev.target.value)}/> 
+               <button onClick={() => {
+                        setSend(input)
+                        setInput('')
+                }}>Buscar</button>
+            </div>
+            <div>
+            {send && <SearchDigimon digimon={send} />}
+            </div>
+            <hr />
+        </div>
+    )
+}
+
+function SearchDigimon(props){
+    const [digimonData, setDigimonData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://digimoncard.io/api-public/search.php?n=' + props.digimon)
+        .then(response => response.data)
+        .then(data => setDigimonData(data))
+        .catch(error => console.error('Error', error))            
+    }, [props.digimon])
+
+    return <div>
+        <ul>
+            {digimonData.map((value, index) => (
+                <li key={index}>
+                    <img src={value.img} alt="Digimon" />
+                   <h2>{value.name}</h2>
+                   <p>{value.cardnumber}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+}
+
+function DigimonAPI(){
+    const [digimonData, setDigimonData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://digimoncard.io/api-public/getAllCards.php?sort=name&series=Digimon Card Game&sortdirection=asc')
+        .then(response => response.data)
+        .then(data => setDigimonData(data))
+        .catch(error => console.error('Error', error))            
+    }, [])
+
+    return <div>
+        <ul>
+            {digimonData.map((value, index) => (
+                <li key={index} className='digimonPreencher'>
+                   <h2>{value.name}</h2>
+                   <p>{value.cardnumber}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+}
+
+function InputValidado(){
+    const [exInput, setExInput] = useState(''),
+          [search, setSearch] = useState([]),
+          [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <div>
+               <input type="text" value={exInput} onChange={(ev) => setExInput(ev.target.value)}/> 
+               <button onClick={() => {
+                    if (exInput.length > 3){
+                        setCount(count + 1)
+                        setSearch([...search, {valor: exInput, num: count}])
+                        setExInput('')
+                    }
+               }}>Adicionar</button>
+            </div>
+            {exInput.length <= 3 && exInput.length != 0 && (
+                <h1 className='erro'>Pesquisa muito curta.</h1>
+            )}
+            <Item vetor={search}/>
+        </div>
+    )
+}
+
+function Item(props){
+
+    return <div>
+        <h1>Lista</h1>
+        <ul>
+            {props.vetor.map((valor, index) => (
+                <li key={index} style={valor.num % 2 == 0 ? {background: "red"} : {background: "green"}}>
+                    {valor.valor}
+                </li>
+            ))}
+        </ul>
+    </div>
 }
 
 export default App
