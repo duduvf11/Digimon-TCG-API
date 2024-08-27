ce209483aa451afc1bbed67ef5ee7abc740080a0
rodrigoaleixoff <rodrigo.aff22@gmail.com>
2024-08-27 09:53:49 -0300

    att auth

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -1,8 +1,24 @@
+import "dotenv/config"
+
 import jsonwebtoken from "jsonwebtoken"
-const { decode } = jsonwebtoken
+const { verify } = jsonwebtoken
 
 export default function(req, res, next){
 
-    
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) return res.status(401).json({message: "Token invalida"})
+
+    const [, token] = split(' ')
+
+    try{
+
+        verify(token, process.env.JWT_SECRET)
+
+
+    } catch(err){
+
+        return res.status(401).end()
+    }
 
 }
