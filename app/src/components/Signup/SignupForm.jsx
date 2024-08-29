2f0ec4f10cc46e69fb7c3fadece0b4f3bb0a141b
Eduardo Viegas Francisco <duduviegasfrancisco11@gmail.com>
2024-08-29 18:24:30 -0300

    signup done

--- app/src/components/Signup/SignupForm.jsx
+++ app/src/components/Signup/SignupForm.jsx
@@ -1,24 +1,63 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import "./SignupForm.css";
 import { Link } from 'react-router-dom';
+import axios from "axios"
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 
 import { ThemeContext } from '../../context/ThemeContext';
 
 const SignupForm = () => {
-  const {theme} = useContext(ThemeContext)
+    const {theme} = useContext(ThemeContext);
+      
+    // Estado para armazenar username e password
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [alertMessage, setAlertMessage] = useState(null);
+    const [alertVariant, setAlertVariant] = useState(''); 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:3030/user/new', { user, password })
+        .then(() => {
+            setAlertMessage('Cadastro bem-sucedido!');
+        })
+        .catch(error => {
+            setAlertMessage('Erro ao fazer Cadastro. Verifique suas credenciais.');
+            setAlertVariant('danger');
+            console.error('Erro ao fazer cadastro:', error);
+        });
+    };
+
 
   return (
-    <Form className='Signup_container' data-bs-theme={theme}>
+    <Form className='Signup_container' data-bs-theme={theme} onSubmit={handleSubmit}>
+    {alertMessage && (
+      <Alert variant={alertVariant} onClose={() => setAlertMessage(null)} dismissible>
+          {alertMessage}
+      </Alert>
+    )}
+
       <Form.Group className="mb-3" controlId="formBasicUsername">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="username" placeholder="Enter Username"/>
+            <Form.Control 
+                    type="text" 
+                    placeholder="Enter Username"
+                    value={user}
+                    onChange={(e) => setUser(e.target.value)}
+            />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control 
+                    type="password" 
+                    placeholder="Password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
       </Form.Group>
       <Button variant="primary" type="submit">
         Register
