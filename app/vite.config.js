0d4743527fe482835ee1ec2a454ae62551f17854
Eduardo Viegas Francisco <duduviegasfrancisco11@gmail.com>
2024-08-30 19:46:11 -0300

    vite compression settings

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip', // ou 'brotliCompress'
+      ext: '.gz', // extensão do arquivo comprimido
+    }),
+  ],
   base: "/Digimon-TCG-API"
 })
